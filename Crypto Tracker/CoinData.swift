8e70fea6dcb1026a994678f5381d28522b04ba35
Irina <zhogova.irina1@yandex.ru>
2018-06-28 15:54:28 +0300

    Создание CryptoTableViewController и NavigationController без использования storyboard + CoinData

---
+++ Crypto Tracker/CoinData.swift
@@ -0,0 +1,17 @@
+//
+//  CoinData.swift
+//  Crypto Tracker
+//
+//  Created by Ирина Жогова on 28.06.2018.
+//  Copyright © 2018 Irina Zhogova. All rights reserved.
+//
+
+import UIKit
+
+class Coin {
+    var symbol = ""
+    var image = UIImage()
+    var price = 0.0
+    var amount = 0.0
+    var historicalData = [Double]()
+}
